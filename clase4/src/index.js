fe8e1215f5d160122395839b9a4bdb8e847d6ef6
andy <arteysoft@gmail.com>
2023-05-17 21:56:28 -0300

    clase #4

--- clase4/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const faker = require('faker'); // CommonsJS
-const uuid_1 = require("uuid");
-let a = 3;
-a = 4;
-console.log(a);
-console.log(faker.name.firstName());
-console.log((0, uuid_1.v4)());
--- clase4/src/index.ts
+++ clase4/src/index.ts
@@ -1,6 +1,22 @@
 const faker = require('faker'); // CommonsJS
 import {v4 as uuid} from 'uuid'
+import fs from 'fs'
 
 import genUsuario from './lib/genusuario'
+import { Usuario } from './interfaces/Usuario';
 
-console.log(genUsuario())
+let fnGrabar = (iteracion:number) => {
+    let usu:Usuario = genUsuario()
+    console.log(usu)
+
+    fs.writeFile('/var/arch/uno.json', JSON.stringify(usu), 'utf-8', err => {
+        console.log(err?err:'')
+        fnGrabar(iteracion + 1)
+    })
+}
+
+fnGrabar(1)
+
+/* SI SE ANIMAN, PUEDEN TRABAJARLO */
+/// Lo que falta es que ? primero un corte y segundo que el archivo se deberia llamar diferente cada vez
+
