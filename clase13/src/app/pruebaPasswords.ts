f7b75777b856c49336ff99331f7206c2fc04573b
andy <arteysoft@gmail.com>
2023-06-26 21:45:01 -0300

    ejercicio 2

--- clase13/src/app/pruebaPasswords.ts
+++ clase13/src/app/pruebaPasswords.ts
@@ -1,16 +1,32 @@
 import sha256 from 'sha256'
 import {v4 as uuid} from 'uuid'
-import {crearUsuario, validarUsuario} from '../lib/manejadorSQLUsuarios'
+import {crearUsuario, validarUsuario, crearToken} from '../lib/manejadorSQLUsuarios'
 
-export default () => {
+export default async () => {
 
     /*
-    crearUsuario({usuario:'max', passwordClear: 'max33RedBull'}, (err) => {
-        console.log(err)
-    })
+    crearUsuario({usuario:'valtery', passwordClear: 'Mercedes77'})
+    .then(() => { console.log('usuario creado') })
+    .catch(err => { console.log(err) })
+    */
+    
+    /* PROBAMOS POR THEN Y CATCH
+    validarUsuario('max', 'max33RedBull')
+    .then(() => { console.log('ok max')})
+    .catch(err => console.log(err))
+
+    validarUsuario('valtery', 'Mercedes77')
+    .then(() => { console.log('ok valtery')})
+    .catch(err => console.log(err))
     */
 
-    validarUsuario('max', 'max33RedBull', (err) => {
+    try {
+        await validarUsuario('valtery', 'Mercedes77')
+        let token = 'unToken__' + uuid()
+        console.log(token)
+        await crearToken('valtery', token)
+    }
+    catch (err) {
         console.log(err)
-    })
+    }
 }
--- clase13/src/lib/manejadorSQLUsuarios.ts
+++ clase13/src/lib/manejadorSQLUsuarios.ts
@@ -1,90 +1,50 @@
 import * as todoElDriver from 'mysql2'
-import {crearConexion} from './obtenerConexion'
+import {crearConexion, connect, insert, query, cerrarConexion} from './driverPromise'
 import {v4 as uuid} from 'uuid'
 import sha256 from 'sha256'
 
-let cerrarConexion = (conn, onFinish) => {
-    conn.end(err => {
-        if (err) {
-            onFinish(err)
-            return
-        }
-        onFinish(null)
-    })
-}
-
-export let crearUsuario = (usuarioDto, onFinish) => {
+export let crearUsuario = async (usuarioDto) => {
     let connection = crearConexion()
-
-    connection.connect(err => {
-        if (err) {
-            onFinish(err)
-            return
-        }
-    })
+    await connect(connection)
 
     usuarioDto.salt = [uuid(), uuid()].join('__')
-    // usuarioDto.usuario
-    // usuarioDto.passwordClear
     let sinEncriptar = [usuarioDto.passwordClear, usuarioDto.salt].join('__')
     console.log(sinEncriptar)
     usuarioDto.passwordEncriptada = sha256(sinEncriptar)
 
     let arrInsert = [uuid(), usuarioDto.usuario, usuarioDto.passwordEncriptada, usuarioDto.salt, '']
+    let strSQL = 'INSERT INTO usuarios (id_usuario, nombre_usuario, password_encriptada, salt, token) VALUES (?, ?, ?, ?, ?)'
 
-    connection.query('INSERT INTO usuarios (id_usuario, nombre_usuario, password_encriptada, salt, token) VALUES (?, ?, ?, ?, ?)', 
-        arrInsert, err => {
-        if (err) {
-            onFinish(err)
-            return
-        }
-        cerrarConexion(connection, onFinish)
-    })
-
-
+    await insert(connection, strSQL, arrInsert)
+    await cerrarConexion(connection)
 }
 
-export let validarUsuario = (usuario, passwordEnClear, onFinish) => {
+export let validarUsuario = async (usuario, passwordEnClear) => {
     let connection = crearConexion()
+    await connect(connection)
+    let resultSet:any = await query(connection, 'SELECT * FROM usuarios where nombre_usuario = ? ', [usuario])
+    console.log(resultSet)
+    await cerrarConexion(connection)
 
-    connection.connect(err => {
-        if (err) {
-            onFinish(err)
-            return
-        }
-    })
+    if (resultSet.length === 0) {
+        throw new Error('La combinacion de usuario y password es erronea:404 Not found')
+    }
 
-    connection.query('SELECT * FROM usuarios where nombre_usuario = ? ', [usuario], (err, results:any, fields) => {
-        if (err) {
-            onFinish(err)
-            return
-        }
-        
-        console.log(results)
-        if (results.length === 0) {
-            cerrarConexion(connection, () => {
-                onFinish(new Error('no vino ningun resultado'))
-            })
-            return
-        }
+    let resultado = resultSet[0]
+    let paraComparar = sha256([passwordEnClear, resultado.salt].join('__'))
 
-        let resultado = results[0]
-
-        let paraComparar = sha256([passwordEnClear, resultado.salt].join('__'))
-
-        console.log(paraComparar)
-        console.log(resultado.password_encriptada)
-
-        if (paraComparar === resultado.password_encriptada) {
-            onFinish('Token')
-            cerrarConexion(connection, onFinish)
-            return
-        }
-
-        cerrarConexion(connection, onFinish)
-    })
+    if (paraComparar !== resultado.password_encriptada) {
+        throw new Error('La clave no coincide')
+    }
 }
 
+export let crearToken = async (usuario, token) => {
+    let connection = crearConexion()
+    await connect(connection)
+    let strSQL = "UPDATE usuarios set token = ? where nombre_usuario = ?"
+    await insert(connection, strSQL, [token, usuario])
+    await cerrarConexion(connection)
+}
 
 export let validarToken = () => {
 
