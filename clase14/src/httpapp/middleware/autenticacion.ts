b259ead8334d7799cb41e1ab47044fcc790e56bb
andy <arteysoft@gmail.com>
2023-07-03 21:54:43 -0300

    clase #14

--- clase14/src/httpapp/middleware/autenticacion.ts
+++ clase14/src/httpapp/middleware/autenticacion.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import {verificarToken} from '../../lib/jwt/verificador'
 
 export default () => (request, response, next) => {
     console.log('ACA HABRIA QUE CHEQUEAR SI VIENE UN TOKEN Y SI NO VIENE VA 401 o 403')
@@ -7,15 +8,14 @@ export default () => (request, response, next) => {
         return
     }
 
-    /*
-        ACA HAY QUE HACER LO MISMO QUE HICIMOS EN CLASE ************************** 2023 06 26
-    */
-
-    
-
-    if (request.headers['x-token'] === 'teMandoElToken') {
+    try {
+        verificarToken(request.headers['x-token'])
+        request.miNumeroDeSuscriber = 123456789
         next()
         return
     }
+    catch (err) {
+
+    }    
     response.status(401).send()
 }
--- clase14/src/httpapp/routes/login.ts
+++ clase14/src/httpapp/routes/login.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import {v4 as uuid} from 'uuid'
+import {crearJWT} from '../../lib/jwt/creador'
 
 export default express.Router()
     .use('', (request, response, next) => {
@@ -14,11 +15,12 @@ export default express.Router()
         // evaluar el body 
         console.log(request.body)
 
-        /*
-        ACA HAY QUE HACER LO MISMO QUE HICIMOS EN CLASE ************************** 2023 06 26
-        */
+        // SELECT FROM USUARIOS WHERE nombre_usuario = ? 
+        // La password que nos envian sumarle el salt y encriptar todo
+        // se comparan las passwords, si son iguales ? la password es correcta
 
-        response.status(200).send({token: uuid()})
+        // en ese caso voy a generar el JWT
+        response.status(200).send({token: crearJWT()})
     })
     .put('', (request, response) => {})
     .delete('', (request, response) => {})
